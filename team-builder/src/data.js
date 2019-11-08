dff3c7e1970c5127745cb8f4a9778ba7749e7ea1
mjedwards <jairedwards20@gmail.com>
2019-11-07 23:40:03 -0500

    completed

---
+++ team-builder/src/data.js
@@ -0,0 +1,27 @@
+export const teamMembers = [
+  {
+    id: 0,
+    name: "Mike",
+    email: "hhhhhhh@gmail.com",
+    role: "Backend Developer"
+  },
+  {
+    id: 1,
+    name: "Jan",
+    email: "jjjjjjjj@gmail.com",
+    role: "Frontend Developer"
+  },
+  {
+    id: 2,
+    name: "Terrell",
+    email: "ttttttt@gmail.com",
+    role: "Fullstack Developer"
+  },
+  { id: 3, name: "Lisa", email: "lllllll@gmail.com", role: "iOS Developer" },
+  {
+    id: 4,
+    name: "Jodie",
+    email: "jojojojojoj@gmail.com",
+    role: "Data Scientist"
+  }
+];
